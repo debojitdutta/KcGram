944d5f58383258c7e3841945b123b0899500bbfd
Debojit Dutta <155905079+debojitdutta@users.noreply.github.com>
2026-09-10 12:19:52 +0530

    Updated and fixed few things

---
+++ homepagescript.js
@@ -0,0 +1,28 @@
+var settingsmenu = document.querySelector(".profile-settings");
+var darkbtn = document.getElementById("dark-btn");
+function settingsMenuToggle() {
+    settingsmenu.classList.toggle("profile-settings-height");
+}
+darkbtn.onclick = function () {
+    darkbtn.classList.toggle("dark-btn-on");
+    document.body.classList.toggle("dark-theme");
+    if(localStorage.getItem("theme")=="light"){
+        localStorage.setItem("theme","dark");
+    }
+    else{
+        localStorage.setItem("theme","light");
+    }
+}
+
+if(localStorage.getItem("theme")=="light"){
+    darkbtn.classList.remove("dark-btn-on-");
+    document.body.classList.remove("dark-theme");
+}
+else if(localStorage.getItem("theme")=="dark"){
+    darkbtn.classList.add("dark-btn-on-");
+    document.body.classList.add ("dark-theme");
+}
+else{
+    localStorage.setItem("theme", "light");
+}
+
